d7cb82c88912d285b7b138543a7808ab9d676eb7
stephen-torosh <98554933+stephen-torosh@users.noreply.github.com>
2024-10-08 16:00:20 +0300

    081024END

--- hello.py
+++ hello.py
@@ -1,30 +1,18 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-code = '''
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@app.route("/")
+def hello_world():
+    return '''
 
     <h1>Hello World!</h1>
     <h3>Welcome to my site!!</h3>
 
-</body>
-</html>
+    '''
 
-<style>
-
-</style>
-
-'''
-
-@app.route("/")
-def hello_world():
-    return code
+@app.route('/example/<text>')
+def hello_world2(text):
+    return f"<h1>just example</h1><h3>but stil the same site :D</h3><h3>{escape(text)}</h3>"
